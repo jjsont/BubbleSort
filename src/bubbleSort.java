009737ebda6149c118adc84df65e6d55aceb06dc
BT_1N3_04@BKTECH.LOCAL <BT_1N3_04@BKTECH.LOCAL>
2019-01-03 14:48:33 -0500

    Initial commit

---
+++ src/bubbleSort.java
@@ -0,0 +1,17 @@
+public class bubbleSort {
+
+    public static void bubbleSort(int[] arr) {
+    boolean swapped=false;
+    while(swapped) {
+        for(int i=0; i<arr.length -1; i++) {
+            for(int j=0; j<arr.length-1; i++) {
+            if (arr[i] >arr[i+1]) {
+                helper.swap(arr, i ,j);
+                swapped=true;
+
+            }
+        }
+    }
+
+    }
+}}
