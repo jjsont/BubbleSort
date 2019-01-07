4c2fab59a8563df69333a42ab06c1d59a9277cb1
BT_1N3_04 <jtan5662@bths.edu>
2019-01-07 14:46:36 -0500

    aaa

--- src/bubbleSort.java
+++ src/bubbleSort.java
@@ -14,4 +14,29 @@ public class bubbleSort {
     }
 
     }
+    }
+
+
+    public static boolean isSorted(int [] arr) {
+        for(int i=0; i<arr.length-1;i++) {
+            if(arr[i]>arr[i+1]) {
+                return false;
+            }
+        }
+        return true;
+        }
+    public static boolean checkSum(int []before, int[]after) {
+        int sum1=0;
+        int sum2=0;
+        if(before.length == after.length) {
+            for(int i=0; i<before.length;i++) {
+                sum1= sum1+before[i];
+                sum2= sum2+ after[i];
+            }
+        }
+        else {
+            return false;
+        }
 }}
+
+
--- src/helper.java
+++ src/helper.java
@@ -23,4 +23,7 @@ public class helper {
         return array;
 
     }
+
+
+
 }
